9fabe372a5394dab3013f96bcfd66b1e91efc5bc
alex snow <alexsnow348@gmail.com>
2024-04-21 17:08:17 +0200

    removed the extra requirement file

---
+++ kedro-dvc/spaceflights-pandas/src/spaceflights_pandas/pipelines/model_training/pipeline.py
@@ -0,0 +1,10 @@
+"""
+This is a boilerplate pipeline 'model_training'
+generated using Kedro 0.19.4
+"""
+
+from kedro.pipeline import Pipeline, pipeline
+
+
+def create_pipeline(**kwargs) -> Pipeline:
+    return pipeline([])

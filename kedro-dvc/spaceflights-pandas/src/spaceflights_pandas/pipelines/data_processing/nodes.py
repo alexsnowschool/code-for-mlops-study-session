51f666e87ba87cc8a760c3fccea4c2b0097e974c
alex snow <alexsnow348@gmail.com>
2024-04-27 17:11:29 +0200

    updated for dvc

--- kedro-dvc/spaceflights-pandas/src/spaceflights_pandas/pipelines/data_processing/nodes.py
+++ kedro-dvc/spaceflights-pandas/src/spaceflights_pandas/pipelines/data_processing/nodes.py
@@ -1,21 +1,5 @@
 import pandas as pd
-
-
-def _is_true(x: pd.Series) -> pd.Series:
-    return x == "t"
-
-
-def _parse_percentage(x: pd.Series) -> pd.Series:
-    x = x.str.replace("%", "")
-    x = x.astype(float) / 100
-    return x
-
-
-def _parse_money(x: pd.Series) -> pd.Series:
-    x = x.str.replace("$", "").str.replace(",", "")
-    x = x.astype(float)
-    return x
-
+from .utils import (_is_true, _parse_percentage, _parse_money)
 
 def preprocess_companies(companies: pd.DataFrame) -> pd.DataFrame:
     """Preprocesses the data for companies.
--- kedro-dvc/spaceflights-pandas/src/spaceflights_pandas/pipelines/model_training/pipeline.py
+++ kedro-dvc/spaceflights-pandas/src/spaceflights_pandas/pipelines/model_training/pipeline.py
@@ -3,8 +3,10 @@ This is a boilerplate pipeline 'model_training'
 generated using Kedro 0.19.4
 """
 
+from venv import create
 from kedro.pipeline import Pipeline, pipeline
 
 
 def create_pipeline(**kwargs) -> Pipeline:
     return pipeline([])
+
